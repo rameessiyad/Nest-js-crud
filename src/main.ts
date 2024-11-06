e06d739b4f66cf09f98ab5a24ff61f93a3dbebbc
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-06 12:02:28 +0530

    server setup done

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app
+    .listen(process.env.PORT ?? 3000)
+    .then(() => console.log('server started on port 3000'));
 }
 bootstrap();
