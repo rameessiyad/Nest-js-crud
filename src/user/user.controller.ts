8a2b5ebc6c4754eef7d26203d0be04ffe7db550f
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-13 12:21:27 +0530

    user controller completed

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createUser(
+    @Body() data: { name: string; email: string; password: string },
+  ) {
+    return this.userService.createUser(data);
+  }
+
+  @Get()
+  async getUsers() {
+    return this.userService.getUsers();
+  }
+
+  @Get(':id')
+  async getUserById(id: number) {
+    return this.userService.getUserById(id);
+  }
+
+  @Patch(':id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() data: { name?: string; email?: string; password?: string },
+  ) {
+    return this.userService.updateUser(+id, data);
+  }
+
+  @Delete(':id')
+  async deleteuser(@Param('id') id: string) {
+    return this.userService.deleteuser(+id);
+  }
+}
